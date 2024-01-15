e37b1e28a89921314dbd1ab4fa57c7ae554f6208
AgamjotSB <agamjotking@gmail.com>
2024-01-16 02:53:31 +0530

    setting up uploadthing

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// shadcn
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// chatgpt
+export function isBase64Image(imageData: string) {
+  const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
+  return base64Regex.test(imageData);
 }
