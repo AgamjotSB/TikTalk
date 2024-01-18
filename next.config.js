4dc68a9e0e8851ffb7dd511ef01912ced46c2311
AgamjotSB <agamjotking@gmail.com>
2024-01-18 11:42:58 +0530

    Fixed profile and profile update

--- next.config.js
+++ next.config.js
@@ -13,7 +13,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.clerk.dev'
-      }
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
     ]
   }
 }
